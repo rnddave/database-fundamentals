abcdf14002e4dddfa410a94d80ec57d3812fe20b
David Dickinson <36966460+rnddave@users.noreply.github.com>
2022-10-04 18:30:55 +0100

    market.sql how to sort by asscending or descending order

--- market.sql
+++ market.sql
@@ -3,9 +3,9 @@ create table market(product char(20), qty_Purchased int(5), qty_Sold int(5), pri
 -- let's populate the table with some info
 
 insert into market values('pepsi', 500, 137, 0.38, 0.65),
-('Dr Pepper', 1000, 997, 0.38, 0.65),
-('7Up', 400, 372, 0.38, 0.65),
-('Lilt', 700, 137, 0.38, 0.65),
+('Dr Pepper', 1000, 997, 0.37, 0.65),
+('7Up', 400, 372, 0.48, 0.65),
+('Lilt', 700, 137, 0.28, 0.65),
 
 -- we want to see any products with less than 5 left in stock
 
@@ -15,3 +15,11 @@ select product, qty_Purchased-qty_Sold AS 'Stock' from market WHERE qty_Purchase
 -- price sold - price purchased
 
 select product, qty_Sold AS 'Items Sold', (price_sold-price_purchased)*qty_Sold AS 'profit' from market
+
+-- lets order data in our reports (defaul is asscending order)
+
+select * from market order by qty_Purchased
+
+-- lets order data in our reports (in descending order)
+
+select * from market order by product DESC
