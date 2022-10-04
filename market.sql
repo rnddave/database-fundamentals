80f4afd6ca616ae4f5dab9bde9f205c6b47403cc
David Dickinson <36966460+rnddave@users.noreply.github.com>
2022-10-04 17:47:58 +0100

    created a new table called markets, added stock and then performed a query to show me stock with less than 5 remaining

---
+++ market.sql
@@ -0,0 +1,12 @@
+create table market(product char(20), qty_Purchased int(5), qty_Sold int(5), price_purchased numeric(6,2), price_sold numeric(6,2))
+
+-- let's populate the table with some info
+
+insert into market values('pepsi', 500, 137, 0.38, 0.65),
+('Dr Pepper', 1000, 997, 0.38, 0.65),
+('7Up', 400, 372, 0.38, 0.65),
+('Lilt', 700, 137, 0.38, 0.65),
+
+-- we want to see any products with less than 5 left in stock
+
+select product, qty_Purchased-qty_Sold AS 'Stock' from market WHERE qty_Purchased-qty_Sold <5
